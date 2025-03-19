21eae16f76e266c166df1de8ec0e066dc5af2b4a
Mark Mcloughlin <mark25mky@gmail.com>
2025-03-19 18:43:49 +0000

    updated views

--- mywebsite/main/views.py
+++ mywebsite/main/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'home.html')
+def index(request):
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def contact(request):
+    return render(request, 'projects.html')    
